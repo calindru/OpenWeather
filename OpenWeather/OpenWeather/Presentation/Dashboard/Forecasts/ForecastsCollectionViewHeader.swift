3896b64377c56e08db1fb80cd25d19760f2e53cf
Calin Drule <12518062+calindru@users.noreply.github.com>
2018-01-07 01:53:28 +0200

    Fixed forecasts reloading when switching data sources

--- OpenWeather/OpenWeather/Presentation/Dashboard/Forecasts/ForecastsCollectionViewHeader.swift
+++ OpenWeather/OpenWeather/Presentation/Dashboard/Forecasts/ForecastsCollectionViewHeader.swift
@@ -14,6 +14,7 @@ class ForecastsCollectionViewHeader: UICollectionReusableView, UICollectionViewD
     
     var forecastsViewModel: ForecastsViewModeling? {
         didSet {
+            forecastDayCollectionView.reloadData()
             forecastDayCollectionView.dataSource = self
         }
     }
